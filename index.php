2100c828ae5405b4731c2f56a6e9f32ed4b53692
RenatoHiga <renatoka14@gmail.com>
2020-02-15 07:58:26 -0300

    Inclusao de varios arquivos

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
     include('includes/config.php');
 
-    //session_destroy(); LOGOUT
+    session_destroy(); //LOGOUT
 
     if (isset($_SESSION['userLoggedIn'])) {
         $userLoggedIn = $_SESSION['userLoggedIn'];
@@ -26,6 +26,46 @@
             <div id="topContainer">
                 
                 <div id="navbarContainer">
+
+                    <nav class="navBar">
+
+                        <a href="index.php" class="logo">
+                            <img src="assets/images/icons/logo-dog.png">
+                        </a>
+
+                        <div class="group">
+
+                            <div class="navItem">
+
+                                <a href="search.php" class="navItemLink">Search</a>
+
+                            </div>
+
+                        </div>
+
+                        <div class="group">
+
+                            <div class="navItem">
+
+                                <a href="browse.php" class="navItemLink">Browse</a>
+
+                            </div>
+                            
+                            <div class="navItem">
+
+                                <a href="yourMusic.php" class="navItemLink">Your music</a>
+
+                            </div>
+
+                            <div class="navItem">
+
+                                <a href="profile.php" class="navItemLink">Renato Higa Higuti</a>
+
+                            </div>
+
+                        </div>
+
+                    </nav>
 
                 </div>
             
